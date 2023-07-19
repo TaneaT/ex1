1f452c50f3102c22e538f9e3245a15c3c585e5dc
TaneaT <trifon.tanea03@gmail.com>
2023-07-19 21:32:20 +0300

    Modified

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,42 @@
 package org.example;
 
-
 import java.util.Scanner;
 
 public class Main {
+    public static int countTheHighestBirthdayCakeCandles(int[] candles) {
+
+        int maxHeightOfCandles = 0;
+        int numberOfTheHighestCandles = 0;
+
+        for (int candle : candles) {
+
+            if (candles[candle] > maxHeightOfCandles) {
+                maxHeightOfCandles = candles[candle];
+
+            }
+        }
+        for (int candle : candles) {
+            if (candles[candle] == maxHeightOfCandles) {
+                numberOfTheHighestCandles++;
+            }
+        }
+        return numberOfTheHighestCandles;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        BirthdayCake birthdayCake = new BirthdayCake();
-
-        int n;
-
         System.out.println("Enter the number of candles: ");
-        n = scanner.nextInt() ;
+        int numberOfCandles = scanner.nextInt();
 
-        int candles[] = new int[n];
+        int[] candles = new int[numberOfCandles];
 
         System.out.println("Enter the high of the candles");
-        for (int i = 0; i<n;i++){
+        for (int i = 0; i < numberOfCandles; i++) {
             candles[i] = scanner.nextInt();
         }
 
-        System.out.println(birthdayCake.birthdayCakeCandles(candles));
+        System.out.println(countTheHighestBirthdayCakeCandles(candles));
 
     }
 }
